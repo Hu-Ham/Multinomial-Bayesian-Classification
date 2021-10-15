d371f6f56f0d644bb540c223c686ab71d84d6949
ngchampion <92596645+ngchampion@users.noreply.github.com>
2021-10-15 12:56:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print("Hello World")
 print("This is another Hello World")
+import json
+
+data = [json.loads(line) for line in open('News_Category_Dataset_v2.json', 'r')]
+for i in data:
+    print(i)
+        
